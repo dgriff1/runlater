c5f1743de975bf62a221aaddf1b175fcaae58c18
mike <mike@mike-VirtualBox.(none)>
2012-07-18 20:09:49 -0500

    Displaying Jobs

--- resources/public/include/js/util.js
+++ resources/public/include/js/util.js
@@ -1,4 +1,46 @@
 
+function renderJobs()
+{
+			$.ajax({
+					url: "jobs/",
+					type: "GET",
+					contentType: 'application/json',
+					error: function(XMLHttpRequest, textStatus, errorThrown){
+					    alert(errorThrown);
+					}, success: function(data, textStatus, XMLHttpRequest){
+						  	buildTable(JSON.parse(XMLHttpRequest.responseText));
+					}
+				    });
+}
+
+function buildTable(results)
+{
+	var table='<table width="100%" border="1">';
+	
+	table+='<tr>';
+	table+='<td></td>';
+	table+='<td>NAME</td>';       
+	table+='<td>STATUS</td>';       
+	table+='<td>URL</td>';       
+	table+='<td>INTERVAL</td>';       
+	table+='<td>WHEN</td></tr>';       
+	for(var i = 0; i < results.length; i++)
+	{
+		table+='<tr>';
+		table+='<td><input type="checkbox" id="'+results[i]._id+'"></td>';       
+		table+='<td>'+results[i].name+'</td>';    
+		table+='<td>'+results[i].status+'</td>';    
+		table+='<td>'+results[i].url+'</td>';    
+		table+='<td>'+results[i].internal+'</td>';    
+		table+='<td>'+results[i].when+'</td>';    
+		table+='</tr>';
+	}
+	table+='</table>';
+	 
+
+	$(".tableWrapper").html( table );	
+}
+
 function testTest()
 {
 	alert("DD");
