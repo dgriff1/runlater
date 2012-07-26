69280c4991401489d6c77c6649a3b69f11abd589
mike <mike@mike-VirtualBox.(none)>
2012-07-26 01:04:49 -0500

    Attempting HMAC security.

--- resources/public/include/js/util.js
+++ resources/public/include/js/util.js
@@ -1,19 +1,20 @@
 
+var userID = "500f8353e4b0fe671fc74d65";
+var publicKey = "prodkey";
+var privateKey = "Ft[7JfgICm2n";
 
 function renderJobs()
 {
-		var userID = "500f8353e4b0fe671fc74d65";
-		var publicKey = "prodkey";
-		var privateKey = "Ft[7JfgICm2n";
-		var privateKey = encode64(CryptoJS.SHA1(privateKey, ""));
+			var hash = CryptoJS.HmacSHA1(privateKey, "");
 
+			hash.toString(CryptoJS.enc.Base64);	
 			$.ajax({
 					headers: {
 						"runlater_key" : publicKey,
-						"runlater_hash" : privateKey,
+						"runlater_hash" : hash,
 						"Content-Type" : "application/json"
 					},
-					url: "jobs/" + userID + "/",
+					url: "users/" + userID + "/jobs/",
 					type: "GET",
 					contentType: 'application/json',
 					error: function(XMLHttpRequest, textStatus, errorThrown){
@@ -54,10 +55,48 @@ function buildTable(results)
 
 function closeJob()
 {
-	$(".addJobDialog").dialog();
+	$(".addJobDialog").find("[name=name]").val("");
+	$(".addJobDialog").find("[name=url]").val("");
+	$(".addJobDialog").dialog('close');
 }
 
 function addJob()
+{
+	var name = $(".addJobDialog").find("[name=name]").val();
+	var url = $(".addJobDialog").find("[name=url]").val();
+	data = ' { ';
+	data += '"name" : "' + name + '" , ';
+	data += '"when" : "2012-05-06T06:15:42.215Z", ';
+	data += '"interval" : "2 hours",';
+	data += '"url" : "' + url + '" , ';
+	data += '"method" : "POST" , ';
+	data += '"headers" : ""'; 
+	data += ' } ';
+
+	var hash = CryptoJS.HmacSHA1(privateKey, data);
+
+	hash.toString(CryptoJS.enc.Base64);	
+
+	$.ajax({
+			headers: {
+				"runlater_key" : publicKey,
+				"runlater_hash" : hash,
+				"Content-Type" : "application/json"
+			},
+			url: "users/" + userID + "/jobs/",
+			type: "PUT",
+			data: data,
+			contentType: 'application/json',
+			error: function(XMLHttpRequest, textStatus, errorThrown){
+			    alert(errorThrown);
+			}, success: function(data, textStatus, XMLHttpRequest){
+					buildTable(JSON.parse(XMLHttpRequest.responseText));
+			}
+		    });
+
+}
+
+function showJobDialog()
 {
 	$(".addJobDialog").dialog({"width" : "400px", "title" : "Add Job"});
 }
