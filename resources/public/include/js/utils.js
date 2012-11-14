e433d749f0da964c39f99fdcf52123cca82959e1
RUNLATER <runlater@runlater-AY747AAR-ABA-p6310y.(none)>
2012-11-13 21:24:22 -0600

    dialog top

--- resources/public/include/js/utils.js
+++ resources/public/include/js/utils.js
@@ -536,7 +536,7 @@ function nameCleaner(e)
 
 function showText(val)
 {
-	$("div[name*=info]").dialog({dialogClass : "alert", modal : true, width: "500px", buttons:{ "Ok": function(){ $(this).dialog("close")}}});
+	$("div[name*=info]").dialog({position: 'top', dialogClass : "alert", modal : true, width: "500px", buttons:{ "Ok": function(){ $(this).dialog("close")}}});
 	$("div[name*=info]").siblings('.ui-dialog-titlebar').remove();
 	$("div[name*=info]").html(textJobs[val]);
 }
